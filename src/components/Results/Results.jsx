60f697daf142589f2064d6c628c72e4dcfa7b12d
cataru25 <cataru25@gmail.com>
2021-09-07 21:38:01 -0500

    Workshop sprint-3, second part

--- src/components/Results/Results.jsx
+++ src/components/Results/Results.jsx
@@ -1,26 +1,26 @@
-import React from 'react'
-import Gif from '../Gif/Gif';
+import React from "react";
+import Gif from "../Gif/Gif";
 
-function Results({
-    data
-}) {
-    return (
+function Results({ data }) {
+  return (
+    <>
+      {!data.length ? (
+        <p>No hay resultados...</p>
+      ) : (
         <section>
-            {(data || []).map(item => {
-                const { id, images: {
-                    fixed_height: {
-                        url
-                    }
-                } } = item;
-                return (
-                    <Gif 
-                        key={id}
-                        src={url} 
-                    />
-                )
-            })}
+          {(data || []).map((item) => {
+            const {
+              id,
+              images: {
+                fixed_height: { url },
+              },
+            } = item;
+            return <Gif key={id} src={url} />;
+          })}
         </section>
-    )
+      )}
+    </>
+  );
 }
 
 export default Results;
--- src/utils/request.js
+++ src/utils/request.js
@@ -1,1 +1,1 @@
-export const request = (resource, query) => fetch(`https://api.giphy.com/v1/gifs${resource}?api_key=gZlFYwmC7QZSHq5OnQu5R8tsSwQn9GP3&q=${query}&limit=25&offset=0&rating=g&lang=en`);
+export const request = (resource, query, limit=12) => fetch(`https://api.giphy.com/v1/gifs${resource}?api_key=gZlFYwmC7QZSHq5OnQu5R8tsSwQn9GP3&q=${query}&limit=${limit}&offset=0&rating=g&lang=en`);
